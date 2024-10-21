1d89029384b36ba5abca8c235569cf0e4277dd24
Alex Bull <alexander.bull24@imperial.ac.uk>
2024-10-21 14:44:42 +0100

    reset app.py line length to 79 max.

--- app.py
+++ app.py
@@ -13,14 +13,26 @@ def submit():
     input_name = request.form.get("name")
     input_animal = request.form.get("animal")
     if input_animal == "Goat" or input_animal == "goat":
-        return render_template("goat.html", name=input_name, animal=input_animal)
+        return render_template(
+            "goat.html", name=input_name, animal=input_animal
+        )
     elif input_animal == "Dog" or input_animal == "dog":
-        return render_template("dog.html", name=input_name, animal=input_animal)
+        return render_template(
+            "dog.html", name=input_name, animal=input_animal
+        )
     elif input_animal == "Donkey" or input_animal == "donkey":
-        return render_template("donkey.html", name=input_name, animal=input_animal)
+        return render_template(
+            "donkey.html", name=input_name, animal=input_animal
+        )
     elif input_animal == "Cow" or input_animal == "cow":
-        return render_template("cow.html", name=input_name, animal=input_animal)
+        return render_template(
+            "cow.html", name=input_name, animal=input_animal
+        )
     elif input_animal == "Lion" or input_animal == "lion":
-        return render_template("lion.html", name=input_name, animal=input_animal)
+        return render_template(
+            "lion.html", name=input_name, animal=input_animal
+        )
     else:
-        return render_template("error.html", name=input_name, animal=input_animal)
+        return render_template(
+            "error.html", name=input_name, animal=input_animal
+        )
